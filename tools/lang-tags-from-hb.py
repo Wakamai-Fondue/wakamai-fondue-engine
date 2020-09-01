835cfb9f810ce16846c475214e78c5d071c1b627
Roel Nieskens <roel@pixelambacht.nl>
2020-09-01 09:35:55 +0200

    Add link to HarfBuzz file that'll be 100% parsable
    In case future changes break this script, there's always a pointer
    to a file that works.

--- tools/lang-tags-from-hb.py
+++ tools/lang-tags-from-hb.py
@@ -4,7 +4,9 @@
 #
 # Usage: ./lang-tags-from-hb.py hb-ot-tag-table.hh
 #
-# Get the table from HarfBuzz:
+# File should be formatted as in this version:
+# https://github.com/harfbuzz/harfbuzz/blob/7a961692e9568806221de8b2e2bf41bdfc1b8b3f/src/hb-ot-tag-table.hh
+# Get the latest table from HarfBuzz:
 # https://raw.githubusercontent.com/harfbuzz/harfbuzz/master/src/hb-ot-tag-table.hh
 #
 # The generated ot-to-html-lang.js file is used by
