b0ca12d87dc3e7519a014a354a863907dd168b33
Pascal Widdershoven <pascal@kabisa.nl>
2020-10-21 19:08:53 +0200

    Remove font.js fork

--- third_party/font.js/lib/context.js
+++
@@ -1,3 +0,0 @@
-// Simple helper variable to distinguish between browser and Node, and values respectively a "window" or a "global" object
-export const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined' ? true : false;
-export const context = isBrowser ? window : global;
