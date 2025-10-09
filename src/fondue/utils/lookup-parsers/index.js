269d05e1528fcc18b3c93864b14af8f4a4176878
Roel Nieskens <roel@pixelambacht.nl>
2025-10-09 09:52:23 +0200

    Add type 2 lookup

--- src/fondue/utils/lookup-parsers/index.js
+++ src/fondue/utils/lookup-parsers/index.js
@@ -1,4 +1,5 @@
 export { parseLookupType1 } from "./type1.js";
+export { parseLookupType2 } from "./type2.js";
 export { parseLookupType3 } from "./type3.js";
 export { parseLookupType4 } from "./type4.js";
 export { parseLookupType6 } from "./type6.js";
