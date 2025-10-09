ffe68781276fe8a7eec7e597b7529722c362c584
Roel Nieskens <roel@pixelambacht.nl>
2025-10-09 10:17:41 +0200

    Recycle type 1 lookup code

--- src/fondue/utils/lookup-parsers/type2.js
+++ src/fondue/utils/lookup-parsers/type2.js
@@ -1,21 +1,3 @@
-import { mergeUniqueCoverage } from "../lookup-utils.js";
+import { parseLookupType1 } from "./type1.js";
 
-// Multiple substitution
-export function parseLookupType2(lookup, charFor, charactersFromGlyphs) {
-	const parsedData = {
-		input: [],
-		backtrack: [],
-		lookahead: [],
-		alternateCount: [],
-	};
-
-	lookup.subtableOffsets.forEach((_, i) => {
-		const subtable = lookup.getSubTable(i);
-		const coverage = subtable.getCoverageTable();
-		const results = charactersFromGlyphs(coverage, charFor);
-
-		parsedData.input = mergeUniqueCoverage(parsedData.input, results);
-	});
-
-	return parsedData;
-}
+export const parseLookupType2 = parseLookupType1;
